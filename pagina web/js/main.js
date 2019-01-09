71548e4bc67f77f31c6d6264f8828bee0ccec04e
Rubén <42714332+MiYazJE@users.noreply.github.com>
2019-01-09 19:32:34 +0100

    Add files via upload

---
+++ pagina web/js/main.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+
+	$(window).on('scroll', function() {
+
+		if($(window).scrollTop()) {
+			$('nav').addClass('black');
+		}
+		else {
+			$('nav').removeClass('black');			
+		}
+		
+	});
+
+});
+
+
