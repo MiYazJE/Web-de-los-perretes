87ae8de5324159d5676223c62b54f1905747e221
MiYazJE <ruben.saizserrano@gmail.com>
2019-01-30 05:02:59 +0100

    close

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
 $( document ).ready(function() {
 
-	$(window).on('scroll', function() {
-		if($(window).scrollTop() > 100) {
-			$('nav').addClass('black');
-		}
-		else {
-			$('nav').removeClass('black');			
-		}
-
-		if($(window).scrollTop() > 500) {
-			$('.icon-bar').fadeIn(500);
-		}
-		else {
-			$('.icon-bar').fadeOut(500);
-		}
-	});
-
 	$('.slider').bxSlider({
 		responsive: true,
 		slideWidth: 600,
@@ -24,6 +8,31 @@ $( document ).ready(function() {
 		auto: true,
 		speed: 1000,
 		adaptiveHeight: true,
+	});
+
+	$(window).on('scroll', function() {
+
+		if($(this).scrollTop() > 100) {
+			$('nav').addClass('black');
+		} else {
+			$('nav').removeClass('black');			
+		}
+
+		if($(this).scrollTop() > 500) {
+			$('.icon-bar').fadeIn(500);
+			$('.boton-ir-arriba').fadeIn(500);
+		} else {
+			$('.icon-bar').fadeOut(500);
+			$('.boton-ir-arriba').fadeOut(500);
+		}
+
+		$('.boton-ir-arriba').click(function() {
+			$('body, html').animate({
+				scrollTop: '0px'
+			}, 500);
+			return false;
+		});
+
 	});
 
 });
