a3e7b7a839943605efff63c45e6e195fa2ada7a7
MiYazJE <ruben.saizserrano@gmail.com>
2019-02-03 17:00:25 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,40 +1,24 @@
-$( document ).ready(function() {
+$(window).on('scroll', function() {
 
-	$('.slider').bxSlider({
-		responsive: true,
-		slideWidth: 600,
-		controls: false,
-		pager: false,
-		auto: true,
-		speed: 1000,
-		adaptiveHeight: true,
-	});
+	if($(this).scrollTop() > 100) {
+		$('nav').addClass('black');
+	} else {
+		$('nav').removeClass('black');			
+	}
 
-	$(window).on('scroll', function() {
+	if($(this).scrollTop() > 500) {
+		$('.icon-bar').fadeIn(500);
+		$('.boton-ir-arriba').fadeIn(500);
+	} else {
+		$('.icon-bar').fadeOut(500);
+		$('.boton-ir-arriba').fadeOut(500);
+	}
 
-		if($(this).scrollTop() > 100) {
-			$('nav').addClass('black');
-		} else {
-			$('nav').removeClass('black');			
-		}
-
-		if($(this).scrollTop() > 500) {
-			$('.icon-bar').fadeIn(500);
-			$('.boton-ir-arriba').fadeIn(500);
-		} else {
-			$('.icon-bar').fadeOut(500);
-			$('.boton-ir-arriba').fadeOut(500);
-		}
-
-		$('.boton-ir-arriba').click(function() {
-			$('body, html').animate({
-				scrollTop: '0px'
-			}, 500);
-			return false;
-		});
-
+	$('.boton-ir-arriba').on('click', function() {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 500);
 	});
 
 });
-
 
